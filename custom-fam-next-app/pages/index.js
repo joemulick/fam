021856aad5ba845972d52d4fd977223939c4c2d9
Joe <joemulick@gmail.com>
2020-10-14 00:00:42 -0700

    removed firebase - installing mongodbatlas

--- custom-fam-next-app/pages/index.js
+++ custom-fam-next-app/pages/index.js
@@ -1,8 +1,9 @@
-import Head from 'next/head'
+import Head from 'next/head';
 import { makeStyles } from '@material-ui/core/styles';
 import styles from '../styles/Home.module.css'
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
+import ReactPlayer from 'react-player/youtube';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,8 +24,14 @@ export default function Home() {
       <Container maxWidth="sm">
         <div className={classes.root}>
           <Grid container spacing={3}>
-            <Grid item xs={12} mb={4}>
-              <h1>Top Box</h1>
+            <Grid item xs={12} m={4}>
+            <ReactPlayer
+              
+              url={[
+                'https://www.youtube.com/watch?v=oUFJJNQGwhk',
+                'https://www.youtube.com/watch?v=jNgP6d9HraI'
+              ]}
+            />
             </Grid>
             <Grid item xs={12}>
               <h1>Lower Box</h1>
